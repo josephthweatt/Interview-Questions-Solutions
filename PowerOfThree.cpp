93f7dd3a88d64b9d9f420844f95054133c3babe1
josephthweatt <josephthweatt@gmail.com>
2016-04-08 16:31:25 -0700

    Initial Commit: Solved PowerOfThree (Leetcode #326)

---
+++ PowerOfThree.cpp
@@ -0,0 +1,25 @@
+/*******************************************************************************
+* Given an integer, write a function to determine if it is a power of three.
+* Follow up:
+* Could you do it without using any loop / recursion?
+*******************************************************************************/
+
+#include <Math.h>
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n == 0) {
+            return false;
+        } else {
+            double x;
+            if (modf(log((double) n) / log(3.0), &x) > .9999999999) {
+                x++;
+            }
+            if (pow (3.0, x) == n) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
