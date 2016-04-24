d919eaed1a8e2890c404045595f625019bd28608
josephthweatt <josephthweatt@gmail.com>
2016-04-23 20:28:42 -0700

    quickly finished a reverse string question with a runtime of 4ms

---
+++ ReverseString.c
@@ -0,0 +1,11 @@
+char* reverseString(char* s) {
+    int size = strlen(s);
+    char temp;
+    printf("%d", size);
+    for (int i = 0; i < size/2; i++) {
+        temp = s[i];
+        s[i] = s[size - 1 - i];
+        s[size - 1 - i] = temp;
+    }
+    return s;
+}
