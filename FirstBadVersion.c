3c553053aa7a5d21f475932315581c601ba006c4
Joseph Thweatt <josephthweatt@gmail.com>
2016-07-01 11:06:52 -0700

    Solved #278 in 0.5 ms
    This question is an example of the importance of binary search

--- FirstBadVersion.c
+++ FirstBadVersion.c
@@ -1,3 +1,21 @@
+/**************************************************************
+ * Written by: Joseph Thweatt
+ * Runtime: 0.5 ms
+ * You are a product manager and currently leading a team to 
+ * develop a new product. Unfortunately, the latest version of 
+ * your product fails the quality check. Since each version is 
+ * developed based on the previous version, all the versions 
+ * after a bad version are also bad.
+ * 
+ * Example: Suppose you have n versions [1, 2, ..., n] and you 
+ * want to find out the first bad one, which causes all the 
+ * following ones to be bad.
+ * 
+ * Note: This was solved using a binary search method. Linear
+ * search for the same question would have caused the question
+ * to time out.
+ *************************************************************/
+
 // Forward declaration of isBadVersion API.
 bool isBadVersion(int version);
 
@@ -6,22 +24,21 @@ int firstBadVersion(int n) {
         return 1;
     }
     
-    int i;
-    int num = 1, den = 2;
+    long int i;
+    long int num = 1, den = 2;
     while (true) {
-        i = (int) (n * (((double) num) / den));
-        printf("%d %d %d %d\n", num, den, n, (int) (n * (((double) num) / den)));
+        i = (int) (n * (((long double) num) / den));
         if (isBadVersion(i)) {
             if (!isBadVersion(i - 1)) {
                 printf("%d", i);
                 return i;
             } else {
-                num += (num*2-1 > 1) ? num*2-1 : 1;
+                num = (num*2-1 > 1) ? num*2-1 : 1;
                 den = den * 2;
             }
         } else {
             den = den * 2;
-            if (((num*2+1)/den)*n == i) {
+            if ((int) ((((long double) num*2+1)/den)*n) == i) {
                 num = den;
             } else {
                 num = num*2+1;
