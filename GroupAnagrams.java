63d1a31c92ce684cccbf467d601a0e536d716c7e
JosephThweatt <josephthweatt@gmail.com>
2018-08-15 15:35:54 -0700

    Solved #49 Group Anagrams

---
+++ GroupAnagrams.java
@@ -0,0 +1,28 @@
+/*
+ * Written by: Joseph Thweatt
+ * Runtime: 308 ms
+ * 
+ * Given an array of strings, group anagrams together.
+ */
+import java.util.*;
+
+public class GroupAnagrams {
+
+	public List<List<String>> groupAnagrams(String[] strs) {
+		Map<String, List<String>> letterSets = new HashMap<>();
+		for (String s : strs) {
+			char[] chars = s.toCharArray();
+			Arrays.sort(chars);
+			String letterSet = new String(chars);
+
+			if (!letterSets.containsKey(letterSet)) {
+				letterSets.put(letterSet, new ArrayList<>());
+			}
+			letterSets.get(letterSet).add(s);
+		}
+
+		return new ArrayList(letterSets.values());
+	}
+
+}
+
